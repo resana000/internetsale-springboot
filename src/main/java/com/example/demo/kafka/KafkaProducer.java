58f39a3ab8fa6f48d87d233a6f91f1907c2b7fd2
user <user@yser>
2020-05-08 18:55:35 +0300

    add kafka

---
+++ src/main/java/com/example/demo/kafka/KafkaProducer.java
@@ -0,0 +1,18 @@
+package com.example.demo.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaProducer {
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+
+    }
+}
